e7ffba90748a31601e357cfd4797f7ec2c764a5b
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-03-11 09:01:01 +0530

    initiate

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+    return (
+        //Bem
+        <div className="app">
+            <h1>Hey Clever Programmer, Let's build Airbnb Clone</h1>
+            {/*<Home/>*/}
+            {/*<Header/>*/}
+            {/*<Banner/>*/}
+            {/*Search*/}
+            {/*<Cards/>*/}
+            {/*<Footer/>*/}
+            {/*SearchPage*/}
+            {/*...*/}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDmpbtnvCdIhL3z2g56Z4AvuXFEnKKyML8",
+    authDomain: "airbnb-shanu.firebaseapp.com",
+    projectId: "airbnb-shanu",
+    storageBucket: "airbnb-shanu.appspot.com",
+    messagingSenderId: "272862971923",
+    appId: "1:272862971923:web:46a03c012aa31a4a0c17e6"
+};
