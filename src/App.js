5a22fc70cd6fb41022d14646f68e3bdc04dab752
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-03-13 15:32:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React from 'react';
 import Home from './Home';
 import Header from "./Header";
-
+import Footer from "./Footer";
 const App = () => {
     return (
         //Bem
         <div className="app">
             <Header/>
             <Home/>
+            <Footer/>
 
             {/*<Banner/>*/}
             {/*Search*/}
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+    return (<div className="footer">
+        <p>© 2021 Airbnb, Inc. No rights reserved - this is a demo</p>
+        <p>Privacy·Terms·Sitemap·Company details</p>
+    </div>)
+}
+export default Footer;
